7dbd006627d97bda62a77de1bc70b6577dd7134d
clarkj99 <clark@stontech.net>
2019-11-16 16:01:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = 'Clarke';
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+excitedWelcomeMessage = welcomeMessage.toUpperCase();
+shortGreeting = 'Welcome, ' + currentUser.slice(0, 1) + '!';
