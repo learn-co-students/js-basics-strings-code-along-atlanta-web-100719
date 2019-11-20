8bfa941ea3c7aa620e422a93654557975811b834
Dan Reeves <you@example.com>
2019-11-20 03:12:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+let currentUser = 'Grace Hopper!';
+
+    let welcomeMessage = 'Welcome to Flatbook, ';
+    welcomeMessage += currentUser;
+
+    let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ';
+
+    excitedWelcomeMessage += currentUser.toUpperCase();
+
+    let shortGreeting = 'Welcome, ';
+
+    shortGreeting += currentUser.charAt(0) + '!';
